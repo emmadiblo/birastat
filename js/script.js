a0417c7b57a46b3451c01a6c1b3b07306ae0979b
emmadiblo <141231760+emmadiblo@users.noreply.github.com>
2024-12-07 19:22:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1111,7 +1111,7 @@ handlePlaybackError(error) {
     }
 
     // Afficher le message d'erreur
-    this.elements.errorMessage.textContent = `Impossible de lire cette station. Erreur : ${error.message || error.code}`;
+    this.elements.errorMessage.textContent =errorMessage;
 
     this.elements.errorMessage.style.display = 'block';
     this.updatePlayState();
